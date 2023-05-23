b0534f6efc3bdd84fe21bb1a7958951f98638c5c
Augs Machado <augustomachadoramos@gmail.com>
2023-05-23 08:51:07 -0300

    feat: add new endpoint to request the api status

--- backend/routes/status.routes.js
+++ backend/routes/status.routes.js
@@ -3,6 +3,7 @@ import StatusCtrl from "../controllers/status.controllers.js";
 
 const router = express.Router();
 
+router.route("/").get(StatusCtrl.getStatusApi);
 router.route("/status").get(StatusCtrl.getStatusApi);
 
 export default router;
